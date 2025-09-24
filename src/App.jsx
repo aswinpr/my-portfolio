f6491c4bab0ce181ea0b0b4453d9d9d3c1565b33
aswinpr <aswinpr46@gmail.com>
2025-09-24 10:49:29 +0530

    test1

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className='header'>
       <h1 className="text-3xl font-bold text-red-500">Vite + React</h1>
+      <h1 className="text-3xl font-bold text-red-500">test</h1>
     </div>
   );
 }
