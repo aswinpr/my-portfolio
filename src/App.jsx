c52f896f10df9d05f922ad0b2bf4ea8aff8b4911
aswinpr <aswinpr46@gmail.com>
2025-09-24 10:44:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='header'>
+      <h1 className="text-3xl font-bold text-red-500">Vite + React</h1>
+    </div>
+  );
+}
+
+export default App;
